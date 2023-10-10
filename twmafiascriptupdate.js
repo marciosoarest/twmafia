558723e3158f26dc007d6c32788518b9971722fa
Marcio Trindade <60787825+marciosoarest@users.noreply.github.com>
2023-10-10 14:41:04 -0300

    Add files via upload

---
+++ twmafiascriptupdate.js
@@ -0,0 +1,8 @@
+var scriptUpdate={
+	TWMAFIA:'3.01',
+	TWFBTNew:'Bug fixes',
+	announcementBlock: '2.85',
+	announcementBlockNew: 'Support for other language servers',
+	WTK: '3.00',
+	WTKNew: 'Extended Item Finder: Create a list of items you want to search for + BB-Codes for color, size and images in telegrams, profiles and notes',
+};
